2cdbd2238361a099a9847028113a746909847160
LAVADPR <lavadpr@oocl.com>
2020-10-15 14:22:01 +0800

    [fizz buzz] [prince] refactor - refactor variables with meaningful name

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,14 +1,21 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+    private final String FIZZ = "Fizz";
+    private final String BUZZ = "Buzz";
+    private final String WHIZZ = "Whizz";
+    private final int FIZZ_MODULO = 3;
+    private final int BUZZ_MODULO = 5;
+    private final int WHIZZ_MODULO = 7;
+
     public String countOff(int orderNumber) {
         String output = "";
-        if(orderNumber%3==0)
-            output += "Fizz";
-        if(orderNumber%5==0)
-            output += "Buzz";
-        if(orderNumber%7==0)
-            return "Whizz";
+        if(orderNumber%FIZZ_MODULO==0)
+            output += FIZZ;
+        if(orderNumber%BUZZ_MODULO==0)
+            output += BUZZ;
+        if(orderNumber%WHIZZ_MODULO==0)
+            return WHIZZ;
         return output.equals("") ? String.valueOf(orderNumber): output;
     }
 }
